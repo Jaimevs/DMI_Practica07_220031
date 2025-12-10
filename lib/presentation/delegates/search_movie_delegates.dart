3595b2bd621b7d64ff1a5eaad569afc2ab8deee4
Jaimevs <jaime.vqzan@gmail.com>
2025-12-09 22:38:03 -0600

    Practica 07
    Creacion de boton de busqeuda de pelicias

--- lib/presentation/delegates/search_movie_delegates.dart
+++ lib/presentation/delegates/search_movie_delegates.dart
@@ -1,13 +1,15 @@
 import 'dart:async';
 
-import 'package:cinemapedia_220031/domain/entities/movie.dart';
+import 'package:cinemapedia_220031/config/helpers/human_formats.dart';
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia_220031/domain/entities/movie.dart';
 
 typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
 
-class SearchMovieDelegate extends SearchDelegate<Movie>{
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
   final SearchMoviesCallback searchMovies;
-  List<Movie> intialMovies;
+  List<Movie> initialMovies;
 
   StreamController<List<Movie>> debouncedMovies = StreamController.broadcast();
   StreamController<bool> isLoadingStream = StreamController.broadcast();
@@ -16,31 +18,32 @@ class SearchMovieDelegate extends SearchDelegate<Movie>{
 
   SearchMovieDelegate({
     required this.searchMovies,
-    required this.intialMovies
-  }):super(
-    searchFieldLabel: 'Buscar peliculas'
+    required this.initialMovies
+  }) : super(
+    searchFieldLabel: 'Buscar películas',
   );
 
-  void clearStreams(){
+  void clearStreams() {
     debouncedMovies.close();
+    isLoadingStream.close();
   }
 
-  void _onQueryChanged(String query){
+  void _onQueryChanged(String query) {
     isLoadingStream.add(true);
 
-    if(_debounceTimer?.isActive ?? false) _debounceTimer!.cancel();
+    if (_debounceTimer?.isActive ?? false) _debounceTimer!.cancel();
 
-    _debounceTimer = Timer(const Duration(milliseconds: 500), () async{
+    _debounceTimer = Timer(const Duration(milliseconds: 500), () async {
       final movies = await searchMovies(query);
-      intialMovies = movies;
+      initialMovies = movies;
       debouncedMovies.add(movies);
       isLoadingStream.add(false);
     });
   }
 
-  Widget buildResultsAndSuggestions(){
+  Widget buildResultsAndSuggestions() {
     return StreamBuilder(
-      initialData: intialMovies,
+      initialData: initialMovies,
       stream: debouncedMovies.stream,
       builder: (context, snapshot) {
         final movies = snapshot.data ?? [];
@@ -49,17 +52,140 @@ class SearchMovieDelegate extends SearchDelegate<Movie>{
           itemCount: movies.length,
           itemBuilder: (context, index) => _MovieItem(
             movie: movies[index],
-            onMovieSelected: (context, movie)
-            {
+            onMovieSelected: (context, movie) {
               clearStreams();
               close(context, movie);
-            }
+            },
           ),
         );
       },
     );
   }
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      StreamBuilder(
+        initialData: false,
+        stream: isLoadingStream.stream,
+        builder: (context, snapshot) {
+          if (snapshot.data ?? false) {
+            return SpinPerfect(
+              duration: const Duration(seconds: 20),
+              spins: 10,
+              infinite: true,
+              child: IconButton(
+                onPressed: () => query = '',
+                icon: const Icon(Icons.refresh_rounded),
+              ),
+            );
+          }
+
+          return FadeIn(
+            animate: query.isNotEmpty,
+            child: IconButton(
+              onPressed: () => query = '',
+              icon: const Icon(Icons.clear),
+            ),
+          );
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        clearStreams();
+        close(context, null);
+      },
+      icon: const Icon(Icons.arrow_back_ios_new_rounded),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return buildResultsAndSuggestions();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    _onQueryChanged(query);
+    return buildResultsAndSuggestions();
+  }
 }
 
-@overide
-Widget b
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+  final Function onMovieSelected;
+
+  const _MovieItem({
+    required this.movie,
+    required this.onMovieSelected
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+    final size = MediaQuery.of(context).size;
+
+    return GestureDetector(
+      onTap: () {
+        onMovieSelected(context, movie);
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: Row(
+          children: [
+            // Image
+            SizedBox(
+              width: size.width * 0.2,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  movie.posterPath,
+                  loadingBuilder: (context, child, loadingProgress) =>
+                      FadeIn(child: child),
+                ),
+              ),
+            ),
+
+            const SizedBox(width: 10),
+
+            // Description
+            SizedBox(
+              width: size.width * 0.7,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(movie.title, style: textStyles.titleMedium),
+
+                  (movie.overview.length > 100)
+                      ? Text('${movie.overview.substring(0, 100)}...')
+                      : Text(movie.overview),
+
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star_half_rounded,
+                        color: Colors.yellow.shade800,
+                      ),
+                      const SizedBox(width: 5),
+                      Text(
+                        HumanFormats.number(movie.voteAverage),
+                        style: textStyles.bodyMedium!.copyWith(
+                          color: Colors.yellow.shade900,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
